69a83e2ba96fbb46d714750170e22026d7e648c9
saroyangor <dr.saroyan98@gmail.com>
2024-06-04 19:29:57 +0300

    fs, path, storage

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
-import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { getArgs } from "./helpers/args.js"
+import { printError, printHelp, printSuccess } from "./services/log.service.js"
+import { saveKeyValue } from "./services/storage.service.js"
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Токен сохранен')
+    } catch (e) {
+        printError(e.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
@@ -12,7 +22,7 @@ const initCLI = () => {
         // city
     }
     if (args.t) {
-        // token
+        return saveToken(args.t)
     }
 }
 
